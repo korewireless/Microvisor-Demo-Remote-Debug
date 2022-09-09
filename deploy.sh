c461817148f24677dfe048403a7f89995d8b1668
Tony Smith <smittytone@icloud.com>
2022-09-09 14:34:50 +0100

    Bug fixes and post-lint updates

--- deploy.sh
+++ deploy.sh
@@ -15,27 +15,28 @@
 app_dir=demo
 app_name=mv-remote-debug-demo.zip
 #------------^ APP SPECIFIC ^------------
-private_key_path="build/${app_dir}/debug_auth_priv_key.pem"
 cmake_path="${app_dir}/CMakeLists.txt"
 zip_path="build/${app_dir}/${app_name}"
+private_key_path=NONE
+public_key_path=NONE
 do_log=0
 do_build=1
 do_deploy=1
 do_update=1
-public_key_path=NONE
+do_gen_keys=0
 output_mode=text
 mvplg_minor_min="3"
-mvplg_patch_min="0"
+mvplg_patch_min="3"
 
 # NOTE
-# This script the build directory is called 'build' and exists within
-# the project directory. If it is used to build the app, this will be
-# the case. You can pass in alternative target for the build product
+# This script assumes the build directory is called 'build' and exists
+# within the project directory. If it is used to build the app, this will
+# be the case. You can pass in alternative target for the build product
 # eg. './deploy.sh build_alt/app/my_app.zip'
 
 # FROM 1.6.0 -- Trap ctrl-c
 stty -echoctl
-trap 'echo Done' SIGINT
+trap 'echo End of Line' SIGINT
 
 # FUNCTIONS
 show_help() {
@@ -43,12 +44,15 @@ show_help() {
     echo -e "  ./deploy.sh /optional/path/to/Microvisor/app/bunde.zip\n"
     echo -e "Options:\n"
     echo "  --log / -l            After deployment, start log streaming. Default: no logging"
-    echo "  --output / -o {mode}  Log output mode: \'text\` or \`json\`"
-    echo "  --public-key {path}   /path/to/remote/debugging/public/key.pem"
-    echo "  --private-key {path}  /path/to/remote/debugging/private/key.pem"
+    echo "  --genkeys             Generate remote debugging keys"
+    echo "  --publickey {path}    /path/to/remote/debugging/public/key.pem"
+    echo "                        Must be a pre-generated file if you do not include --gen-keys"
+    echo "  --privatekey {path}   /path/to/remote/debugging/private/key.pem"
+    echo "                        Must be a pre-generated file if you do not include --gen-keys"
     echo "  --deploy / -d         Deploy without a build"
-    echo "  --log-only            Start log streaming immediately; do not build or deploy"
-    echo "  -h / --help           Show this help screen"
+    echo "  --logonly             Start log streaming immediately; do not build or deploy"
+    echo "  --output / -o {mode}  Log output mode: \'text\` or \`json\`"
+    echo "  --help / -h           Show this help screen"
     echo
 }
 
@@ -58,6 +62,26 @@ stream_log() {
         twilio microvisor:logs:stream "${MV_DEVICE_SID}" --output=json | jq
     else
         twilio microvisor:logs:stream "${MV_DEVICE_SID}"
+    fi
+}
+
+set_keys() {
+    echo -e "Generating Remote Debugging keys..."
+    # Check for passed directories
+    if [[ -d "${private_key_path}" ]]; then
+        private_key_path="${private_key_path}/debug_auth_prv_key.pem"
+    fi
+    
+    if [[ -d "${public_key_path}" ]]; then
+        public_key_path="${public_key_path}/debug_auth_pub_key.pem"
+    fi
+
+    # Generate the keys using the Twilio CLI Microvisor plugin
+    if twilio microvisor:debug:generate_keypair --debug-auth-privkey="${private_key_path}" --debug-auth-pubkey="${public_key_path}" --force ; then
+        echo "Private key written to ${private_key_path}"
+        echo " Public key written to ${public_key_path}"
+    else
+        show_error_and_exit "Could not generate remote debugging keys"
     fi
 }
 
@@ -72,14 +96,14 @@ check_prereqs() {
     [[ ${bv} -lt 4 ]] && show_error_and_exit "This script requires Bash 4+"
 
     #2: required utilities
-    prqs=(jq cmake curl twilio)
+    prqs=(jq cmake twilio)
     for prq in "${prqs[@]}"; do
-        check=$(which "${prq}") || show_error_and_exit "Required utility ${prq} not installed"
+        which "${prq}" > /dev/null 2>&1 || show_error_and_exit "Required utility ${prq} not installed"
     done
 
     #3: credentials set
     [[ -z ${TWILIO_ACCOUNT_SID} || -z ${TWILIO_AUTH_TOKEN} ]] && show_error_and_exit "Twilio credentials not set as environment variables"
-    
+
     #4: Microvisor plugin version
     result=$(twilio plugins | grep 'microvisor' | awk {'print $2'})
     minor=$(echo "$result" | cut -d. -f2)
@@ -94,8 +118,8 @@ build_app() {
     else
         cmake -S . -B build
     fi
-
-    if cmake --build build --clean-first > /dev/null ; then
+    
+    if cmake --build build --clean-first > /dev/null 2>&1 ; then
         echo "App built"
     else
         show_error_and_exit "Could not build the app"
@@ -132,8 +156,8 @@ for arg in "$@"; do
             show_error_and_exit "Missing value for ${last_arg}"
         fi
         case "${arg_is_value}" in
-            1) private_key_path="${arg}" ; echo "Remote Debugging private key: ${private_key_path}" ;;
-            2) public_key_path="${arg}"  ; echo "Remote Debugging public key: ${public_key_path}"   ;;
+            1) private_key_path="${arg}" ;;
+            2) public_key_path="${arg}"  ;;
             3) output_mode="${check_arg}" ;;
             *) echo "[Error] Unknown argument" exit 1 ;;
         esac
@@ -143,11 +167,11 @@ for arg in "$@"; do
 
     if [[ "${check_arg}" = "--log" || "${check_arg}" = "-l" ]]; then
         do_log=1
-    elif [[ "${check_arg}" = "--private-key" ]]; then
+    elif [[ "${check_arg}" = "--privatekey" ]]; then
         arg_is_value=1
         last_arg=${arg}
         continue
-    elif [[ "${check_arg}" = "--public-key" ]]; then
+    elif [[ "${check_arg}" = "--publickey" ]]; then
         arg_is_value=2
         last_arg=${arg}
         continue
@@ -155,11 +179,13 @@ for arg in "$@"; do
         arg_is_value=3
         last_arg=${arg}
         continue
-    elif [[ "${check_arg}" = "--log-only" ]]; then
+    elif [[ "${check_arg}" = "--logonly" ]]; then
         do_log=1
         do_deploy=0
         do_build=0
-        elif [[ "${check_arg}" = "--deploy" || "${check_arg}" = "-d" ]]; then
+    elif [[ "${check_arg}" = "--genkeys" ]]; then
+        do_gen_keys=1
+    elif [[ "${check_arg}" = "--deploy" || "${check_arg}" = "-d" ]]; then
         do_build=0
     elif [[ "${check_arg}" = "--help" || "${check_arg}" = "-h" ]]; then
         show_help
@@ -170,6 +196,35 @@ for arg in "$@"; do
         zip_path="${arg}"
     fi
 done
+
+# FROM 1.7.0 -- generate keys if required
+if [[ ${do_gen_keys} -eq 1 ]]; then
+    if [[ "${public_key_path}" = "NONE" ]]; then
+        # Public key path not specified, so use the default
+        public_key_path="$(pwd)/build/${app_dir}/debug_auth_pub_key.pem"
+    fi
+    
+    if [[ "${private_key_path}" = "NONE" ]]; then
+        # Private key path not specified, so use the default
+        private_key_path="$(pwd)/build/${app_dir}/debug_auth_prv_key.pem"
+    fi
+    
+    # Create the build directory so we can save keys there
+    if [[ ! -e build ]]; then
+        mkdir -p "$(pwd)/build/${app_dir}" || show_error_and_exit "Could not create the build directory"
+    fi
+
+    # Generate the keys
+    set_keys
+else
+    # User didn't ask to generate keys - did they pass in a public key path?
+    if [[ "${public_key_path}" != "NONE" ]]; then
+        # Check key path
+        if [[ ! -f "${public_key_path}" ]]; then
+            show_error_and_exit "Public key cannot be found at ${public_key_path}"
+        fi
+    fi
+fi
 
 # FROM 1.6.0 -- check output mode
 if [[ ${output_mode} != "text" ]]; then
@@ -192,8 +247,7 @@ if [[ ${do_deploy} -eq 1 ]]; then
 
     # Try to upload the bundle
     echo "Uploading ${zip_path}..."
-    upload_action=$(curl -X POST https://microvisor-upload.twilio.com/v1/Apps -H "Content-Type: multipart/form-data" -u "${TWILIO_ACCOUNT_SID}:${TWILIO_AUTH_TOKEN}" -s -F File=@"${zip_path}")
-
+    upload_action=$(twilio microvisor:apps:create "${zip_path}" -o=json)
     app_sid=$(echo "${upload_action}" | jq -r '.sid')
 
     if [[ -z "${app_sid}" || "${app_sid}" == "null" ]]; then
@@ -201,7 +255,7 @@ if [[ ${do_deploy} -eq 1 ]]; then
     else
         # Success... try to assign the app
         echo "Assigning app ${app_sid} to device ${MV_DEVICE_SID}..."
-        if ! assign_action=$(twilio api:microvisor:v1:devices:update --sid=${MV_DEVICE_SID} --target-app=${app_sid}); then
+        if ! twilio api:microvisor:v1:devices:update --sid="${MV_DEVICE_SID}" --target-app="${app_sid}" > /dev/null 2>&1 ; then
             exit 1
         fi
     fi
@@ -211,11 +265,9 @@ fi
 rm -f null.d
 
 # Dump remote debugging command if we can
-echo -e "\nUse the following command to initiate remote debugging:"
-if [[ "${public_key_path}" != "NONE" ]]; then
+if [[ "${public_key_path}" != "NONE" && "${private_key_path}" != "NONE" ]]; then
+    echo -e "\nUse the following command to initiate remote debugging:"
     echo "twilio microvisor:debug ${MV_DEVICE_SID} '${private_key_path}'"
-else
-    echo "twilio microvisor:debug ${MV_DEVICE_SID} '$(pwd)/${private_key_path}'"
 fi
 
 # Start logging if requested to do so
