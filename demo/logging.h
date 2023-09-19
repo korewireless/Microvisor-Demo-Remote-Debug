e5574d7f2207dc3ff1b7a94c08f9567edd75f037
smittytone <tsmith@korewireless.com>
2023-09-19 09:46:40 +0100

    Code tidy

--- demo/logging.h
+++ demo/logging.h
@@ -34,9 +34,9 @@ extern "C" {
 /*
  * PROTOTYPES
  */
-void            server_log(char* format_string, ...)        __attribute__ ((__format__ (__printf__, 1, 2)));
-void            server_error(char* format_string, ...)      __attribute__ ((__format__ (__printf__, 1, 2)));
-void            do_assert(bool condition, char* message);
+void server_log(char* format_string, ...)        __attribute__ ((__format__ (__printf__, 1, 2)));
+void server_error(char* format_string, ...)      __attribute__ ((__format__ (__printf__, 1, 2)));
+void do_assert(bool condition, char* message);
 
 
 #ifdef __cplusplus
