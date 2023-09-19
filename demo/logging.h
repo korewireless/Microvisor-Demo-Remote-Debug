4d020842e44451b51240d17f6875812b2dfbd907
smittytone <tsmith@korewireless.com>
2023-09-19 09:43:40 +0100

    Bump to 3.1.1; better logging compiler warnings

--- demo/logging.h
+++ demo/logging.h
@@ -34,8 +34,8 @@ extern "C" {
 /*
  * PROTOTYPES
  */
-void            server_log(char* format_string, ...);
-void            server_error(char* format_string, ...);
+void            server_log(char* format_string, ...)        __attribute__ ((__format__ (__printf__, 1, 2)));
+void            server_error(char* format_string, ...)      __attribute__ ((__format__ (__printf__, 1, 2)));
 void            do_assert(bool condition, char* message);
 
 
